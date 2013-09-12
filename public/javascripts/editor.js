5cc466fd8645c454b3541d37639db57b4123aa83
kevinychen <binonunquineist@gmail.com>
2013-09-12 13:38:30 -0400

    Some style changes.

--- public/javascripts/editor.js
+++ public/javascripts/editor.js
@@ -1,15 +1,16 @@
 var currentLanguage = "java";
 var editor;
 
-var javaTemplate = "import java.util.*\nimport java.io.*;\n\npublic class Main {\n\npublic static void main (String[] args) throws IOException {\n//your code here\n}\n}";
-var cppTemplate = "#include <stdio.h>\n#include <stdlib.h>\n#include <iostream>\n#include <math.h>\n#include <string.h>\n#include <algorithm>\n#include <vector>\n\nusing namespace std;\n\nint main() {\n\n//your code here\n\n}";
-var pythonTemplate = "//no boilerplate, your code goes here";
+var javaString = "import java.util.*\nimport java.io.*;\n\npublic class Main {\n\npublic static void main (String[] args) throws IOException {\n//your code here\n}\n}";
+var cppString = "#include <stdio.h>\n#include <stdlib.h>\n#include <iostream>\n#include <math.h>\n#include <string.h>\n#include <algorithm>\n#include <vector>\n\nusing namespace std;\n\nint main() {\n\n//your code here\n\n}";
+var pythonString = "//no boilerplate, your code goes here";
 
 $(document).ready(function() {
     // ace-editor
     editor = ace.edit("editor");
     editor.setTheme("ace/theme/monokai");
-    switchLanguage('java');
+    editor.setValue(javaString);
+    editor.getSession().setMode("ace/mode/java");
     document.getElementById('editor').style.fontSize = '14px';
 });
 
@@ -23,14 +24,21 @@ onclickFunction = function() {
 }
 
 switchLanguage = function(language) {
+    if (currentLanguage == 'java') {
+        javaString = editor.getValue();
+    } else if (currentLanguage == 'cpp') {
+        cppString = editor.getValue();
+    } else {
+        pythonString = editor.getValue();
+    }
     if (language == 'java') {
-        editor.setValue(javaTemplate);
+        editor.setValue(javaString);
         editor.getSession().setMode("ace/mode/java");
     } else if (language == 'cpp') {
-        editor.setValue(cppTemplate);
+        editor.setValue(cppString);
         editor.getSession().setMode("ace/mode/c_cpp");
     } else {
-        editor.setValue(pythonTemplate);
+        editor.setValue(pythonString);
         editor.getSession().setMode("ace/mode/python");
     }
     currentLanguage = language;
